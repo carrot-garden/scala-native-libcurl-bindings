3d1558986957f1f39188aca1d88ed37c3c083f24
Felix Satyaputra <felix.satyaputra@typesafe.com>
2017-10-06 15:31:09 +1100

    Comment updates

--- src/main/scala/com/lightbend/scalanative/curl/libcurl.scala
+++ src/main/scala/com/lightbend/scalanative/curl/libcurl.scala
@@ -1,18 +1,19 @@
 package com.lightbend.scalanative.curl
 
 import scala.scalanative.native
-import scala.scalanative.native._
 
 @native.link("curl")
 @native.extern
 object libcurl {
-  type CURL = native.Ptr[Unit]
-  type CURLcode = native.Ptr[Unit]
+  type CURL = native.Ptr[_]
+  type CURLcode = native.Ptr[_]
 
   def curl_easy_init(): CURL = native.extern
 
   /*
    These 2 lines is supposed to represent the following inside curl/curl.h
+
+   #define CURLOPTTYPE_STRINGPOINT   10000
 
    #define CINIT(na,t,nu) CURLOPT_ ## na = CURLOPTTYPE_ ## t + nu
 
@@ -23,14 +24,18 @@ object libcurl {
      // more enum declarations (like 260+ enums) not shown
    } CURLoption;
 
-   Related?: https://github.com/scala-native/scala-native/issues/205
+   The macro above should generate a typedef enum called CURLOPT_URL with the value of 10002.
+
+   I've ran a quick check with https://godbolt.org/g/ZDYwbt and the above seems to be the case.
+
+   Possibly related?: https://github.com/scala-native/scala-native/issues/205
 
    */
   class CURLoption(val value: native.CInt) extends AnyVal
   val CURLOPT_URL: CURLoption = native.extern
 
   /*
-   How do we represent this method, particularly the parameter which can be a long, function pointer, object pointer, or curl_off_t?
+   Is this how do we represent this method, particularly the parameter which can be a long, function pointer, object pointer, or curl_off_t?
 
    From https://curl.haxx.se/libcurl/c/curl_easy_setopt.html
 
